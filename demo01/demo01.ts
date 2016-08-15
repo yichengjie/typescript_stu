745b0d17a877805d5bb136f375037da3fd54cefb
yichengjie <626659321@qq.com>
2016-08-15 21:41:10 +0800

    proj init

---
+++ demo01/demo01.ts
@@ -0,0 +1,13 @@
+'use strict' ;
+
+function greeter(person:string) {
+    return "Hello ," + person ;
+}
+
+var user = "Jane User" ;
+
+var user2 = 3 ;
+
+
+
+document.body.innerHTML = greeter(user) ;
